65d09f33cfa30c8bf6548d99c83b313d1b8ac64c
Akos032 <horako032@hengersor.hu>
2025-02-17 12:15:05 +0100

    Menu elkészitése
    Css-sel menu formázása

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,10 +4,10 @@ import { Link,NavLink } from "react-router-dom";
 const Navbar = (() => {
     return(
         <div>
-            <nav>
-                <Link to ='/'>Főoldal</Link>
-                <NavLink to = '/ermesek'>Az olimpia érmesei</NavLink>
-                <NavLink to = '/mermesek'>A magyar érmese</NavLink>
+            <nav className="dtc v-mid w-75 tr" >
+                <Link className="link dim dark-gray f6 f5-ns dib mr3 mr4-ns" to ='/'>Főoldal</Link>
+                <NavLink className="link dim dark-gray f6 f5-ns dib mr3 mr4-ns" to = '/ermesek'>Az olimpia érmesei</NavLink>
+                <NavLink className="link dim dark-gray f6 f5-ns dib" to = '/mermesek'>A magyar érmese</NavLink>
             </nav>
         </div>
     )
