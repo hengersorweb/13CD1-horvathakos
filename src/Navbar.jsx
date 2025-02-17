a2adf9036c33e1f614727f0933659a05cca9f7c7
Akos032 <horako032@hengersor.hu>
2025-02-17 11:29:06 +0100

    szükséges modulok importálása
    Bootstrap
    Route
    Tachyons

---
+++ src/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = (() => {
+    return(
+        <div>
+            <h1>Az olimpia története</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam accusamus aperiam doloribus reprehenderit molestias architecto ullam error illum, maxime cumque laborum, optio aliquam consequatur. Praesentium, amet. Dicta eos atque esse.</p>
+        </div>
+    )
+})
+
+export default Navbar
---
+++ src/oldalak/Mermesek.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Mermesek = (() => {
+    return(
+        <div>
+            <h1>Az olimpai magyar érmesei</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam accusamus aperiam doloribus reprehenderit molestias architecto ullam error illum, maxime cumque laborum, optio aliquam consequatur. Praesentium, amet. Dicta eos atque esse.</p>
+        </div>
+    )
+})
+
+export default Mermesek
