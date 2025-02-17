df79955baa1ec6464ab024f1f47e8784605ba890
Akos032 <horako032@hengersor.hu>
2025-02-17 11:58:15 +0100

    Router beállitása
    Menu rendszer kialakitása

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link,NavLink } from "react-router-dom";
 
 const Navbar = (() => {
     return(
         <div>
-            <h1>Az olimpia története</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam accusamus aperiam doloribus reprehenderit molestias architecto ullam error illum, maxime cumque laborum, optio aliquam consequatur. Praesentium, amet. Dicta eos atque esse.</p>
+            <nav>
+                <Link to ='/'>Főoldal</Link>
+                <NavLink to = '/ermesek'>Az olimpia érmesei</NavLink>
+                <NavLink to = '/mermesek'>A magyar érmese</NavLink>
+            </nav>
         </div>
     )
 })
